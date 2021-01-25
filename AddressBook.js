d55804de622529b09c3db00f6eb50fcf9d08a934
pradippatil01 <mr.pradip01@gmail.com>
2021-01-25 18:04:25 +0530

    Start: Welcome message display

---
+++ AddressBook.js
@@ -0,0 +1,11 @@
+/**************************************************************************
+*   Excecution : 1. default Node 
+*   Purpose    : AddressBook using Javascript
+*   @description 
+*   @author    : Pradip R patil (BridgeLabz)
+*   @file      : AddressBook.js
+*   @version   : v15.6.0
+***************************************************************************/
+console.log('******************************');
+console.log('    Welcome to AddressBook    ');
+console.log('******************************');
