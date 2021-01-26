8ba9989407e61162630c8959c26e8b8338da2d0c
pradippatil01 <mr.pradip01@gmail.com>
2021-01-26 17:51:15 +0530

    add : for loop added in addPerson to add multiple data

--- AddressBookManage.js
+++ AddressBookManage.js
@@ -9,15 +9,18 @@ class AddressBook {
      * @usecase1 create contact  
      */
     addPerson = () => {
-        let firstName = readline.question('Enter your First Name:');
-        let lastName = readline.question('Enter your last name : ');
-        let city = readline.question('Enter your City : ');
-        let state = readline.question('Enter your State name : ');
-        let zipCode = readline.question('Enter your ZipCode name : ');
-        let phoneNumber = readline.question('Enter your Phone Number : ');
-        let email = readline.question('Enter your Email name : ');
-        var personData = new person(firstName, lastName, city, state, zipCode, phoneNumber, email);
-        readWrite.writeInBook(personData.toString())
+        let number = readline.question('How many data you want to add: ');
+        for (let i = 0; i < number; i++) {
+            let firstName = readline.question('Enter your First Name:');
+            let lastName = readline.question('Enter your last name : ');
+            let city = readline.question('Enter your City : ');
+            let state = readline.question('Enter your State name : ');
+            let zipCode = readline.question('Enter your ZipCode name : ');
+            let phoneNumber = readline.question('Enter your Phone Number : ');
+            let email = readline.question('Enter your Email name : ');
+            var personData = new person(firstName, lastName, city, state, zipCode, phoneNumber, email);
+            readWrite.writeInBook(personData.toString())
+        }
     }
     /**
     * @usecase3 edit contact  
